4df54d690a4ab62fa138b777c3a4e6d34e7719bc
BRYAN JONATAN <bryan.jonatan@binus.ac.id>
2026-04-22 21:40:02 +0700

    hilangin comment

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-# URL Backend SISPARNUS
-# Ganti dengan IP server yang sesuai
-
-# Untuk development/laptop:
-BACKEND_URL = "http://192.168.101.15:5000"
+# URL Backend, Ganti dengan IP server yang sesuai
+# development/laptop:
+BACKEND_URL = "http://localhost:5000"
 
 # Untuk Raspberry Pi (uncomment dan ganti IP):
 # BACKEND_URL = "http://<IP_SERVER>:7186"
